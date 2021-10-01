125fcec6c1596609af5e75c8969d55ab2841670e
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-01 17:31:04 +0200

    first iterations

---
+++ iteration3.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//ITERACIÓN 3
+
+/* 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+
+1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}; */
+
+//1.1
+
+let a = 5;
+let b = 10;
+let c = a * b;
+
+console.log(c);
+
+//1.2
+
+let d = 2;
+let e = b / d;
+
+console.log(e)
+
+//1.3
+let f = 9
+let resto = (a + b) % 9
+console.log(resto)
+
+//1.4
+
+let y = 10;
+let z = 5
+
+let x = y + z;
+console.log(x)
+
+//1.5
+
+x = y * z;
+console.log(x)
